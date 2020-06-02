707bbea9a95821edc910932a93bbcfc05a0c37bb
PusheenMaster5 <66330094+PusheenMaster5@users.noreply.github.com>
2020-06-02 19:27:48 -0400

    Update Farewellcardgen.py

--- Farewellcardgen.py
+++ Farewellcardgen.py
@@ -1,9 +1,10 @@
 #Farewell Card Generator (For CMS) 3 lines
 #add support for people going to the same school as you later
 #License stuffs
+import sys
 License = '''Farewell Card Generator for the AP students of Chesapeake Montessori School
 Distributed under the Modzilla Public License V.2.0
-Created By Justin McCubbin
+(c) 2020, Created By Justin McCubbin
 Dedicated to Hamza Nasher, because you can dedicate code to people now, too (I guess)?'''
 print (License,'\n'*2)
 MyName = input('What is your name? ')
@@ -28,4 +29,10 @@ if TheirGrade == '7':
     Line3 = 'See you next year.'
 else:
     Line3 = 'Hope you do well in High School.'
-print (f'Hi {TheirName},\n{Line1}  {Line2}  {Line3}  \nAll the best, {MyName}')
+#saves output (Data) to a txt file (TheirName.txt)
+Data = (f'Hi {TheirName},\n{Line1}  {Line2}  {Line3}  \nAll the best, {MyName}')
+Datafile = open(f'{TheirName}data.txt', 'w')
+Datafile.write(Data)
+Datafile.close()
+print (Data)
+sys.exit()
