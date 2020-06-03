89c1d61bf1011e0aa5e767fd959cc344a2a1b129
PusheenMaster5 <66330094+PusheenMaster5@users.noreply.github.com>
2020-06-03 14:25:42 -0400

    Update Farewellcardgen.py

--- Farewellcardgen.py
+++ Farewellcardgen.py
@@ -10,31 +10,49 @@ print (License,'\n'*2)
 MyName = input('What is your name? ')
 TheirName = input('Who are you writing to? ')
 MyGrade = input('What is your grade? (7, 8) ')
-TheirGrade = input('What is {}\'s grade? '.format(TheirName))
-Knowledge = input('Did you know them before? (y, n) ')
-SameSchool = input('Are you going to go to the same High School as them? ')
+TeacherStat = input('Are they a teacher? ')
+if TeacherStat == 'y':
+    if MyGrade == '7':
+        Line1 = 'It was great getting to know you this year!'
+        Line3 = 'See you next year'
+    elif MyGrade == '8':
+        Line1 = 'It was great seeing you again this year.'
+        Line3 = 'Hope to see you again.'
+else:
+    Line1 = ''
+    Line3 = ''
+    TheirGrade = input('What is {}\'s grade? '.format(TheirName))
+    Knowledge = input('Did you know them before? (y, n) ')
+    if MyGrade == '8' and TheirGrade == '8':
+        SameSchool = input('Are you going to go to the same High School as them? ')
 Line2 = input('Sentence about you and {}\'s time together. (eg: I enjoyed building a town with you.) '.format(TheirName))
 #if you are new or dont know anybody, it says you get to know them, otherwise, you are 'reconnecting' with them
-if TheirGrade == '7' and Knowledge == 'n':
-    Line1 = 'It was great getting to know you this year!'
-elif TheirGrade == '7' and MyGrade == '8' and Knowledge == 'y':
-    Line1 = 'It was great reconnecting with you this year!'
-elif TheirGrade == '7' and MyGrade == '7' and Knowledge == 'y':
-    Line1 = 'It was great reconnecting with you this year!' 
-elif TheirGrade == '8' and Knowledge == 'n':
-    Line1 = 'It was great getting to know you this year!'
-elif TheirGrade == '8' and Knowledge == 'y':
-    Line1 = 'It was great reconnecting with you this year!'
-    
-if TheirGrade == '7':
-    Line3 = 'See you next year.'
-elif MyGrade == '7' and TheirGrade == '8':
-    Line3 = 'Hope you do well in High School.'
-elif MyGrade == '8' and TheirGrade == '8' and SameSchool == 'y':
-    Line3 = 'See you in High School.'
-elif MyGrade == '8' and TheirGrade == '8' and SameSchool == 'n':
-    Line3 = 'Hope you do well in High School.'
-#saves output (Data) to a txt file (TheirName.txt)
+if Line1 == '' and Line3 == '':
+    if TheirGrade == '7' and Knowledge == 'n':
+        Line1 = 'It was great getting to know you this year!'
+    elif TheirGrade == '7' and MyGrade == '8' and Knowledge == 'y':
+        Line1 = 'It was great reconnecting with you this year!'
+    elif TheirGrade == '7' and MyGrade == '7' and Knowledge == 'y':
+        Line1 = 'It was great reconnecting with you this year!' 
+    elif TheirGrade == '8' and Knowledge == 'n':
+        Line1 = 'It was great getting to know you this year!'
+    elif TheirGrade == '8' and Knowledge == 'y':
+        Line1 = 'It was great reconnecting with you this year!'
+    else:
+        print ('error, restart the program')
+        sys.exit()
+    if TheirGrade == '7':
+        Line3 = 'See you next year.'
+    elif MyGrade == '7' and TheirGrade == '8':
+        Line3 = 'Hope you do well in High School.'
+    elif MyGrade == '8' and TheirGrade == '8' and SameSchool == 'y':
+        Line3 = 'See you in High School.'
+    elif MyGrade == '8' and TheirGrade == '8' and SameSchool == 'n':
+        Line3 = 'Hope you do well in High School.'
+    else:
+        print ('error, restart the program')
+        sys.exit()
+#saves output (Data) to a txt file (TheirNamedata.txt)
 Data = (f'Hi {TheirName},\n{Line1}  {Line2}  {Line3}  \nAll the best, {MyName}')
 Datafile = open(f'{TheirName}data.txt', 'w')
 Datafile.write(Data)
